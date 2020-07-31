e69b2ea35d6923d994368a13dc9b622391c3297c
Ernest Walzel <ewalzel@gmail.com>
2020-07-31 10:05:53 +0200

    Replace X-Frame-Options with X-Content-Security-Policy To allow embedding in slides.com

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,16 @@ Bundler.require(*Rails.groups)
 module VampART
   class Application < Rails::Application
     config.autoload_paths += ["#{config.root}/lib", "#{config.root}/app/services"]
+
+    config.action_dispatch.default_headers = {
+      # Defaults
+      # 'X-Frame-Options' => 'SAMEORIGIN',
+      'X-XSS-Protection' => '1; mode=block',
+      'X-Content-Type-Options' => 'nosniff',
+
+      # Replacement for X-Frame-Options & allow embedding in slides.com
+      'X-Content-Security-Policy' => "frame-ancestors 'self' https://slides.com",
+    }
   end
 end
 
